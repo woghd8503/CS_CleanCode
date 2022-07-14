5655deff99e818256bc860346608e9fe06b7eea3
James <woghd8503@naver.com>
2022-07-15 04:03:13 +0900

    Comments

---
+++ CleanCode/CleanCode/Comments/Comments.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace CleanCode.Comments
+{
+    internal class Comments
+    {
+        private int _pf; // pay frequency
+        private DbContext _dbContext;
+    }
+}
