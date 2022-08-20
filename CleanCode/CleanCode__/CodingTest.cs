af384ce20087dde92aa53f9cc519a02248c5b7ea
James <woghd8503@naver.com>
2022-08-20 17:01:30 +0900

    ClenaCode

---
+++ CleanCode/CleanCode__/CodingTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodingStudy
+{
+    class CodingStudy
+    {
+        static void Main()
+        {
+            bool isLeapYear = false;
+            int input = int.Parse(Console.ReadLine());
+
+            if ((input % 4 == 0 && input % 100 != 0) || (input % 100 == 0 && input % 400 != 0) || input % 400 == 0)
+                isLeapYear = true;
+
+            if (isLeapYear)
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+        }
+    }
+
+
+}
